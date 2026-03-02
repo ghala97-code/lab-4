1ce5ec3e0d9483dac2551a169eaec702e1ff1bfd
ghala97-code <ghala.kr.sh@gmail.com>
2026-03-02 23:58:39 +0300

    lab-4

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -16,16 +16,29 @@ def index2(request, val1 = 0):
   #  return render(request, "bookmodule/index.html")    
 
 
-def index(request):
-    name = request.GET.get("name") or "world!"
-    return render(request, "bookmodule/index.html" , {"name": name})
+#def index(request):
+   # name = request.GET.get("name") or "world!"
+    #return render(request, "bookmodule/index.html" , {"name": name})
 
+
+def index(request):
+    return render(request, "bookmodule/index.html")
+ 
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+ 
 def viewbook(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
+ 
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
+
+#def viewbook(request, bookId):
     
-    book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
-    book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
-    targetBook = None
-    if book1['id'] == bookId: targetBook = book1
-    if book2['id'] == bookId: targetBook = book2
-    context = {'book':targetBook} 
-    return render(request, 'bookmodule/show.html', context)
+ #   book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
+  #  book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
+   # targetBook = None
+    #if book1['id'] == bookId: targetBook = book1
+    #if book2['id'] == bookId: targetBook = book2
+    #context = {'book':targetBook} 
+    #return render(request, 'bookmodule/show.html', context)
