8a154bb23cf8fe6f360e97108e1fe22945bb955e
ghala97-code <ghala.kr.sh@gmail.com>
2026-03-31 09:48:44 +0300

    Lab 5 HTML & CSS Pt 2

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -3,11 +3,15 @@ from . import views
 
 urlpatterns = [
     #path('', views.index),
-    path('index2/<int:val1>/', views.index2),
+    #path('index2/<int:val1>/', views.index2),
     path('<int:bookId>', views.viewbook),
     path('', views.index, name= "books.index"),
     path('list_books/', views.list_books, name= "books.list_books"),
     path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('html5/links/', views.links, name='books.links'),
+    path('html5/text/formatting/', views.text_formatting, name='books.text_formatting'),
+    path('html5/listing/', views.listing, name='books.listing'),
+    path('html5/tables/', views.tables, name='books.tables'),
     path('aboutus/', views.aboutus, name="books.aboutus"),
 
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,44 +1,26 @@
 from django.http import HttpResponse
-from django.shortcuts import render 
-
-# Create your views here.
-
-#def index(request):
- #   name = request.GET.get("name") or "world!"  
-  #  return HttpResponse("Hello, "+name) 
-
-def index2(request, val1 = 0):  
-  return HttpResponse("value1 = "+str(val1))
-
-
-#def index(request): 
- #   name = request.GET.get("name") or "world!"
-  #  return render(request, "bookmodule/index.html")    
-
-
-#def index(request):
-   # name = request.GET.get("name") or "world!"
-    #return render(request, "bookmodule/index.html" , {"name": name})
-
+from django.shortcuts import render
 
 def index(request):
     return render(request, "bookmodule/index.html")
- 
+
 def list_books(request):
     return render(request, 'bookmodule/list_books.html')
- 
+
 def viewbook(request, bookId):
     return render(request, 'bookmodule/one_book.html')
- 
+
 def aboutus(request):
     return render(request, 'bookmodule/aboutus.html')
 
-#def viewbook(request, bookId):
-    
- #   book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
-  #  book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
-   # targetBook = None
-    #if book1['id'] == bookId: targetBook = book1
-    #if book2['id'] == bookId: targetBook = book2
-    #context = {'book':targetBook} 
-    #return render(request, 'bookmodule/show.html', context)
+def links(request):
+    return render(request, 'bookmodule/links.html')
+
+def text_formatting(request):
+    return render(request, 'bookmodule/text_formatting.html')
+
+def listing(request):
+    return render(request, 'bookmodule/listing.html')
+
+def tables(request):
+    return render(request, 'bookmodule/tables.html')
