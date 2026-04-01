72f33a932f0252b5d46f8970fce5ae597d5a7585
ghala97-code <ghala.kr.sh@gmail.com>
2026-04-01 10:57:23 +0300

    Lab 6

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('html5/listing/', views.listing, name='books.listing'),
     path('html5/tables/', views.tables, name='books.tables'),
     path('aboutus/', views.aboutus, name="books.aboutus"),
+    path('search/', views.search_books, name='search_books'),
 
 ]
 
